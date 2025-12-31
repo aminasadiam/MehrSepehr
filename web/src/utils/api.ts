a98a59102c69050392745248c877ab2898395b3a
Amin Asadi <adoramdev@gmail.com>
2025-12-31 15:38:33 +0330

    edit

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,6 +1,6 @@
 // api.ts کامل و اصلاح‌شده
-const API_BASE_URL = "https://mehrsepehr.com:3000/api";
-const BASE_URL = "https://mehrsepehr.com:3000/";
+const API_BASE_URL = "http://mehrsepehr.com:3000/api";
+const BASE_URL = "http://mehrsepehr.com:3000/";
 
 // Utility function to build image URLs
 export const getImageUrl = (path: string | undefined): string => {
